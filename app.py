96f7464ba81c8fbf38268bd1f10189389e47203d
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-26 14:02:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Titre de l'application
 st.title("Mon application de jeu")
-st.image("image.png", caption="Une image pour vous inspirer")
+st.image("image3.png", caption="Une image pour vous inspirer")
 
 # Section pour la création de compte et l'identification
 st.header("Création de compte et identification")
