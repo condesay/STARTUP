8c64f1a3094d22d45c7f3abef23ba6c43677b4b3
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-26 14:23:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,22 +24,54 @@ st.write("Vous avez", age_utilisateur, "ans et vous vous sentez", niveau_anxiete
 
 # Définition des défis en fonction du niveau d'anxiété de l'utilisateur
 if niveau_anxiete == "Pas du tout":
-    st.write("Défi facile : dessinez une image de vous-même")
-    st.write("Défi moyen : écrivez un paragraphe sur votre plus grande peur")
-    st.write("Défi difficile : contactez quelqu'un que vous n'avez pas parlé depuis longtemps")
-    st.write("Défi très difficile : donnez un discours en public")
+    defi_facile = "Défi facile : dessinez une image de vous-même"
+    defi_moyen = "Défi moyen : écrivez un paragraphe sur votre plus grande peur"
+    defi_difficile = "Défi difficile : contactez quelqu'un que vous n'avez pas parlé depuis longtemps"
+    defi_tres_difficile = "Défi très difficile : donnez un discours en public"
 elif niveau_anxiete == "Un peu":
-    st.write("Défi facile : dessinez une image de vous-même")
-    st.write("Défi moyen : écrivez un paragraphe sur votre plus grande peur")
-    st.write("Défi difficile : contactez quelqu'un que vous n'avez pas parlé depuis longtemps")
-    st.write("Défi très difficile : donnez un discours en public") 
+    defi_facile = "Défi facile : dessinez une image de vous-même"
+    defi_moyen = "Défi moyen : écrivez un paragraphe sur votre plus grande peur"
+    defi_difficile = "Défi difficile : contactez quelqu'un que vous n'avez pas parlé depuis longtemps"
+    defi_tres_difficile = "Défi très difficile : donnez un discours en public" 
 elif niveau_anxiete == "Assez":
-    st.write("Défi facile : dessinez une image de vous-même")
-    st.write("Défi moyen : écrivez un paragraphe sur votre plus grande peur")
-    st.write("Défi difficile : contactez quelqu'un que vous n'avez pas parlé depuis longtemps")
-    st.write("Défi très difficile : donnez un discours en public")
+    defi_facile = "Défi facile : dessinez une image de vous-même"
+    defi_moyen = "Défi moyen : écrivez un paragraphe sur votre plus grande peur"
+    defi_difficile = "Défi difficile : contactez quelqu'un que vous n'avez pas parlé depuis longtemps"
+    defi_tres_difficile = "Défi très difficile : donnez un discours en public"
 else:
-    st.write("Défi facile : dessinez une image de vous-même")
-    st.write("Défi moyen : écrivez un paragraphe sur votre plus grande peur")
-    st.write("Défi difficile : contactez quelqu'un que vous n'avez pas parlé depuis longtemps")
-    st.write("Défi très difficile : donnez un discours en public")
+    defi_facile = "Défi facile : dessinez une image de vous-même"
+    defi_moyen = "Défi moyen : écrivez un paragraphe sur votre plus grande peur"
+    defi_difficile = "Défi difficile : contactez quelqu'un que vous n'avez pas parlé depuis longtemps"
+    defi_tres_difficile = "Défi très difficile : donnez un discours en public"
+
+# Section pour les défis
+st.write("Choisissez le défi que vous voulez relever :")
+defi_choisi = st.selectbox("", [defi_facile, defi_moyen, defi_difficile, defi_tres_difficile])
+if st.button("Relever le défi"):
+st.write("Défi relevé avec succès ! Bravo !")
+Section pour les scores
+st.header("Scores")
+score_total = 0
+score_total += 10 # Pour avoir créé un compte
+score_total += 20 # Pour s'être identifié
+score_total += age_utilisateur
+if niveau_anxiete == "Un peu":
+score_total += 20
+elif niveau_anxiete == "Assez":
+score_total += 30
+elif niveau_anxiete == "Très":
+score_total += 50
+st.write("Votre score total est de", score_total, "points.")
+
+Section pour les options de l'application
+st.sidebar.header("Options de l'application")
+option_1 = st.sidebar.checkbox("Option 1")
+option_2 = st.sidebar.checkbox("Option 2")
+option_3 = st.sidebar.checkbox("Option 3")
+if st.sidebar.button("Sauvegarder les options"):
+st.sidebar.write("Options sauvegardées avec succès !")
+
+
+
+
+
